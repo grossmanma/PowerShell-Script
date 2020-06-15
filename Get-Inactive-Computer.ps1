dd0e4146a457d28458b6d9af558538c740e22a04
Matthew Grossman <grossmanma@outlook.com>
2020-06-15 09:48:43 -0500

    Update Get-Inactive-Computer.ps1

--- Get-Inactive-Computer.ps1
+++ Get-Inactive-Computer.ps1
@@ -6,10 +6,10 @@ $archiveP  = ".\archive"
 $timestamp = Get-Date -Format o | foreach {$_ -replace ":", "."}
 #get current date 
 $Now = Get-Date  
-$timespan = New-Timespan –Days 90
-$dtimestamp = New-Timespan –Days 180
-$computers = Search-ADAccount -ComputersOnly –AccountInactive –TimeSpan $timespan | Where {$_.DistinguishedName -notlike "*,OU=DELETE 90 DAYS,*"} 
-$Dcomputers = Search-ADAccount -ComputersOnly –AccountInactive –TimeSpan $timespan | Where {$_.DistinguishedName -like "*,OU=DELETE 90 DAYS,*"} 
+$timespan = New-Timespan –Days 90 # Disable computer object that have not login # days
+$dtimestamp = New-Timespan –Days 180 # Delete computer object that have not login # days
+$computers = Search-ADAccount -ComputersOnly –AccountInactive –TimeSpan $timespan | Where {$_.DistinguishedName -notlike "*,OU=DELETE 90 DAYS,*"} # change OU= to the path in your AD
+$Dcomputers = Search-ADAccount -ComputersOnly –AccountInactive –TimeSpan $timespan | Where {$_.DistinguishedName -like "*,OU=DELETE 90 DAYS,*"} # change OU= to the path in your AD
 #define amount of days to keep backups 
 $Days = "90" 
 #define LastWriteTime parameter based on $Days 
@@ -26,32 +26,32 @@ foreach ($File in $Chkarchive)
 foreach($dcomputer in $dcomputers){
  $data = Get-ADComputer $dcomputer -properties * 
  if($data.OperatingSystem -like "*Windows Server*"){
- Get-ADComputer $dcomputer -properties * | select-object name, SAMaccountname, passwordExpired, PasswordNeverExpires, logoncount, whenCreated, lastlogondate, PasswordLastSet,OperatingSystem  | Export-Csv   .\archive\$DateStamp-YUL-SRV180.csv  -NoTypeInformation -Append
+ Get-ADComputer $dcomputer -properties * | select-object name, SAMaccountname, passwordExpired, PasswordNeverExpires, logoncount, whenCreated, lastlogondate, PasswordLastSet,OperatingSystem  | Export-Csv   .\archive\$DateStamp-SRV180.csv  -NoTypeInformation -Append
  }
  else{
- Get-ADComputer $dcomputer -properties * | select-object name, SAMaccountname, passwordExpired, PasswordNeverExpires, logoncount, whenCreated, lastlogondate, PasswordLastSet,OperatingSystem  | Export-Csv   .\archive\$DateStamp-YUL-NONSRV180.csv  -NoTypeInformation -Append
+ Get-ADComputer $dcomputer -properties * | select-object name, SAMaccountname, passwordExpired, PasswordNeverExpires, logoncount, whenCreated, lastlogondate, PasswordLastSet,OperatingSystem  | Export-Csv   .\archive\$DateStamp-NONSRV180.csv  -NoTypeInformation -Append
      # delete object out of AD
-     #Get-ADComputer $_ | Remove-ADComputer   -Confirm:$false     
+     Get-ADComputer $_ | Remove-ADComputer   -Confirm:$false     
  }}
 
  
  
  
              #sending email with output files 
-if(test-path "C:\script\archive\$DateStamp-YUL-NONSRV180.csv" ){
+if(test-path "C:\script\archive\$DateStamp-NONSRV180.csv" ){
         #Write-Host "this is a test"
-$fromaddress = "donotreply@Yulista.com"  
+$fromaddress = "donotreply@yourdomain.com""  
 
-$toaddress = "Matthew.grossman@yulista.com,Chris.Hunsberger@yulista.com,TSSGroup@Yulista.com,brandon.sparkman@yulista.com,NetServices@yulista.com,jonathan.benge@yulista.com"  
+$toaddress = "someone@yourdomain.com"  
 
 $Subject = "Yulista.us Computer account that need to be delete from AD  Report"  
 
-$attachment = "C:\script\archive\$DateStamp-YUL-SRV180.csv" 
-$attachment1 = "C:\script\archive\$DateStamp-YUL-NONSRV180.csv" 
+$attachment = "C:\script\archive\$DateStamp-SRV180.csv" 
+$attachment1 = "C:\script\archive\$DateStamp-NONSRV180.csv" 
 
   
 
-$smtpserver = "mail.yms-hsv.net"  
+$smtpserver = "SMTP Relay"  
 
 
 ####################################  
@@ -65,7 +65,7 @@ $message.To.Add($toaddress)
 
 $message.Subject = $Subject  
 
-$message.Body = "Attached is the Computer account that need to be delete from AD on Yulista domain. Please review." 
+$message.Body = "Attached is the Computer account that need to be delete from AD on domain. Please review." 
 
 $attach = new-object Net.Mail.Attachment($attachment)  
 
@@ -95,7 +95,8 @@ foreach($computer in $computers){
  Get-ADComputer $computer -properties * | select-object name, SAMaccountname, passwordExpired, PasswordNeverExpires, logoncount, whenCreated, lastlogondate, PasswordLastSet,OperatingSystem  | Export-Csv   .\archive\$DateStamp-YUL-NONSRV90.csv  -NoTypeInformation -Append
  Set-ADComputer $computer -Description "computer was disable by inactive script $DateStamp "
  Disable-ADAccount $computer
- Get-ADComputer $computer | Move-ADObject  -TargetPath 'OU=DELETE 90 DAYS,DC=Yulista,DC=us'
+ Get-ADComputer $computer | Move-ADObject  -TargetPath 'OU=DELETE 90 DAYS,DC=Yulista,DC=us' # change OU= to the path in your AD
+#define amount of days to keep backups 
  }
 
  
@@ -105,18 +106,18 @@ foreach($computer in $computers){
 
  #sending email with output files 
 
-$fromaddress = "donotreply@Yulista.com"  
+$fromaddress = "donotreply@yourdomain.com""  
 
-$toaddress = "Matthew.grossman@yulista.com,Chris.Hunsberger@yulista.com,TSSGroup@Yulista.com,brandon.sparkman@yulista.com,NetServices@yulista.com,jonathan.benge@yulista.com"  
+$toaddress = "someone@yourdomain.com""  
 
-$Subject = "Yulista.us Inactive Computer Report"  
+$Subject = " Inactive Computer Report"  
 
-$attachment = "C:\script\archive\$DateStamp-YUL-NONSRV90.csv" 
-$attachment1 = "C:\script\archive\$DateStamp-YUL-SRV90.csv" 
+$attachment = "C:\script\archive\$DateStamp-NONSRV90.csv" 
+$attachment1 = "C:\script\archive\$DateStamp-SRV90.csv" 
 
   
 
-$smtpserver = "mail.yms-hsv.net"  
+$smtpserver = "SMTP Relay"  
 
 
 ####################################  
@@ -130,7 +131,7 @@ $message.To.Add($toaddress)
 
 $message.Subject = $Subject  
 
-$message.Body = "Attached is the inactive computer account for Yulista log. Please review." 
+$message.Body = "Attached is the inactive computer account log. Please review." 
 
 $attach = new-object Net.Mail.Attachment($attachment)  
 
