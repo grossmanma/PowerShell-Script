aafd55dab7857c1303d470e1e621538305fe3a0c
Matthew Grossman <grossmanma@outlook.com>
2020-06-15 09:59:51 -0500

    Restore delete AD users or just one
    Restore delete AD users or just one

---
+++ Restore-AD-User-Account.ps1
@@ -0,0 +1,23 @@
+﻿ 
+ foreach($user in Get-Content C:\Source\users.txt){
+ Get-ADObject -Filter {sAMAccountName -eq $user} -includeDeletedObjects | Restore-ADObject -NewName $user
+ $userid = Get-ADUser $user
+ Set-ADAccountPassword -Identity $userid -reset -newpassword (ConvertTo-SecureString -AsPlainText "change me!1" -Force) 
+ Set-aduser $userid -changepasswordatlogon $true
+ Enable-ADAccount -Identity $userid
+ $user
+ pause
+
+ }
+
+
+
+ $user = "User "
+Get-ADObject -Filter {sAMAccountName -eq $user} -includeDeletedObjects | Restore-ADObject -NewName $user
+$userid = Get-ADUser $user
+Set-ADAccountPassword -Identity $userid -reset -newpassword (ConvertTo-SecureString -AsPlainText "Yulista2019.!CT" -Force) 
+ Set-aduser $userid -changepasswordatlogon $false
+Enable-ADAccount -Identity $userid
+$user
+ 
+
