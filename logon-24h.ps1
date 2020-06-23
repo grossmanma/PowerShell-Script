5bf0922cc04a7cd0ba22996e89f4e4c653857bcf
Matthew Grossman <grossmanma@outlook.com>
2020-06-23 09:41:18 -0400

    Add files via upload

---
+++ logon-24h.ps1
@@ -0,0 +1,6 @@
+﻿# Defining Time
+$last = (get-date).adddays(-1)
+Get-ADuser -properties * -filter {(lastlogondate -ge $last ) -or (DistinguishedName -notlike "*Service Accounts*") }  |
+select-object name, SAMaccountname, passwordExpired, PasswordNeverExpires, logoncount, whenCreated, lastlogondate, PasswordLastSet,DistinguishedName  | export-csv c:\list\lastlogon.csv -NoTypeInformation 
+
+Send-MailMessage -Attachments C:\list\lastlogon.csv -Subject "users who have login in the last 24 hrs." -To Someone@yourdomain.com -From noreply@yourdomain.com -SmtpServer smtp.yourdomain.com 
