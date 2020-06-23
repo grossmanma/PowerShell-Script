b3b59df9a601ef511ce9058f9a6829661da8f3dc
Matthew Grossman <grossmanma@outlook.com>
2020-06-23 09:38:35 -0400

    Add files via upload

---
+++ resizing-Managed-OS-Disks.ps1
@@ -0,0 +1,19 @@
+﻿# connect to Azure 
+Connect-AzureRmAccount
+Select-AzureRmSubscription –SubscriptionName 'Your Subscription Name'
+
+#select RGS and VM
+$rgName = 'Resource Group'
+$vmName = 'VM Name'
+$vm = Get-AzureRmVM -ResourceGroupName $rgName -Name $vmName
+
+#If VM is running stop it
+Stop-AzureRmVM -ResourceGroupName $rgName -Name $vmName
+
+# change the OS disk Size 
+$disk= Get-AzureRmDisk -ResourceGroupName $rgName -DiskName $vm.StorageProfile.OsDisk.Name
+$disk.DiskSizeGB = 250 # new Size in GB
+Update-AzureRmDisk -ResourceGroupName $rgName -Disk $disk -DiskName $disk.Name
+
+#start VM
+Start-AzureRmVM -ResourceGroupName $rgName -Name $vmName
