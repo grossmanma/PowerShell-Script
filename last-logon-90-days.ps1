da2f3c3d22bb66361674c7c9cc2a53552885f42d
Matthew Grossman <grossmanma@outlook.com>
2020-06-23 09:31:46 -0400

    Add files via upload

---
+++ last-logon-90-days.ps1
@@ -0,0 +1,10 @@
+﻿# Defining Target Path  
+$TargetOU   =  "add target OU"  
+# Defining Time
+$last = (get-date).adddays(-1)
+Get-ADComputer -properties * -filter {(lastlogondate -notlike "*" -OR lastlogondate -le $last) -AND (passwordlastset -le $last) }  |
+select-object name, SAMaccountname, passwordExpired, PasswordNeverExpires, logoncount, whenCreated, lastlogondate, PasswordLastSet, lastlogontimestamp  | export-csv .\lastlogon.csv -NoTypeInformation 
+
+
+
+
